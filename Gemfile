e561e7875b01723cfcbdc074dbc09da4086170b1
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-19 12:02:03 +0200

    added photo feature

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
+gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
+
 # Front End Gems
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3.7'
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,8 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
   validates :name, presence: true, uniqueness: true
 end
