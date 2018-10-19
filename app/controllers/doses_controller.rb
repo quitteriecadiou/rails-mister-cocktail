010d461be54d001495371399c298656567185c2f
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-19 11:04:18 +0200

    destroy method and forms with saved input

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,7 +7,17 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save
+
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
 
     redirect_to cocktail_path(@dose.cocktail)
   end
